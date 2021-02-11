231eb6985ec595d0c581da1c6c3d1edcce814b9a
unknown <mohammed.aseef04@gmail.com>
2021-02-11 20:17:48 +0530

    Deploying Meme App on Heroku

---
+++ src/main/java/com/aseef/meme/aseefcriomemebackend/controller/MemeController.java
@@ -0,0 +1,70 @@
+package com.aseef.meme.aseefcriomemebackend.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aseef.meme.aseefcriomemebackend.model.Meme;
+import com.aseef.meme.aseefcriomemebackend.services.MemeServices;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+//@RequestMapping(value="/api")
+public class MemeController {
+	
+	@Autowired
+	MemeServices myservice ;
+	@CrossOrigin
+	@GetMapping("/memes")
+	@ApiOperation("Return Latest 100 Memes Data in JSON Format")
+	public List<Meme> getMeme() {
+		
+		return myservice.findAllMemes();
+	}
+	@CrossOrigin
+	@GetMapping("/memes/{id}")
+	@ApiOperation("Return Meme with given id in JSON Format")
+	public Meme getMeme(@PathVariable long id) {
+		Meme meme=myservice.getMeme(id);
+		if(meme!=null)
+			return myservice.getMeme(id);
+		return null;
+	}
+	@CrossOrigin
+	@RequestMapping(method=RequestMethod.POST , value="/memes")
+	@ResponseBody
+	@ApiOperation("Adding a Meme in Database and Return respective id of Meme")
+	public HashMap<String, String> addMeme(@RequestBody Meme data) {
+		//Meme meme = new Meme(id,name,caption,url);
+		long id=myservice.addMeme(data).id;
+		HashMap<String, String> map = new HashMap<>();
+		map.put("id", String.valueOf(id));
+		return map;
+	}
+	@CrossOrigin
+	@RequestMapping(method=RequestMethod.PATCH, value="/memes/{id}")
+	@ResponseBody
+	@ApiOperation("Updating a Meme in Database and Return respective id of Meme")
+	public long updateMeme(@PathVariable(value = "id") long id,@RequestBody Meme meme) {
+		
+		myservice.updateMeme(id,meme);
+		return id;
+		
+	}
+	@CrossOrigin
+	@GetMapping("/getlatestid")
+	@ApiOperation("Returns the latest id of Meme")
+	public long getLatestId() {
+		return myservice.getLatestid();
+	}
+}
